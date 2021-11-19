cf7282921bb3396e708c7eb9602da47b0191e397
Pedro <pedralmeida.22.99@gmail.com>
2021-11-19 14:22:01 +0000

    faltou o ficheiro

---
+++ astar.py
@@ -0,0 +1,58 @@
+from heapq import *
+
+
+def heuristic(a, b):
+    return (b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2
+
+
+def astar(start, goal, array, walls):
+    neighbors = [(0, 2), (0, -2), (2, 0), (-2, 0)]
+
+    close_set = set()
+    came_from = {}
+    gscore = {start: 0}
+    fscore = {start: heuristic(start, goal)}
+    oheap = []
+
+    heappush(oheap, (fscore[start], start))
+
+    while oheap:
+
+        current = heappop(oheap)[1]
+
+        if current == goal:
+            data = []
+            while current in came_from:
+                data.append(current)
+                current = came_from[current]
+            # data.append(start)
+            return data
+
+        close_set.add(current)
+        for i, j in neighbors:
+            neighbor = current[0] + i, current[1] + j
+            tentative_g_score = gscore[current] + heuristic(current, neighbor)
+
+            if neighbor not in array:
+                continue
+
+            # Make sure walkable terrain
+            if i == 2 and ((current[0] + 1, current[1]) in walls):
+                continue
+            if i == -2 and ((current[0] - 1, current[1]) in walls):
+                continue
+            if j == 2 and ((current[0], current[1] + 1) in walls):
+                continue
+            if j == -2 and ((current[0], current[1] - 1) in walls):
+                continue
+
+            if neighbor in close_set and tentative_g_score >= gscore.get(neighbor, 0):
+                continue
+
+            if tentative_g_score < gscore.get(neighbor, 0) or neighbor not in [i[1] for i in oheap]:
+                came_from[neighbor] = current
+                gscore[neighbor] = tentative_g_score
+                fscore[neighbor] = tentative_g_score + heuristic(neighbor, goal)
+                heappush(oheap, (fscore[neighbor], neighbor))
+
+    return None
