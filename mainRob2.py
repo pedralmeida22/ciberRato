ee8e1cd3580a12db3927972241cec8290542b87c
Pedro <pedralmeida.22.99@gmail.com>
2021-11-22 00:00:55 +0000

    relatorio

--- mainRob2.py
+++ mainRob2.py
@@ -609,16 +609,19 @@ rob_name = "pClient1"
 host = "localhost"
 pos = 1
 mapc = None
+ficheiro = ""
 
 for i in range(1, len(sys.argv), 2):
     if (sys.argv[i] == "--host" or sys.argv[i] == "-h") and i != len(sys.argv) - 1:
         host = sys.argv[i + 1]
     elif (sys.argv[i] == "--pos" or sys.argv[i] == "-p") and i != len(sys.argv) - 1:
         pos = int(sys.argv[i + 1])
-    elif (sys.argv[i] == "--robname" or sys.argv[i] == "-p") and i != len(sys.argv) - 1:
+    elif (sys.argv[i] == "--robname" or sys.argv[i] == "-r") and i != len(sys.argv) - 1:
         rob_name = sys.argv[i + 1]
     elif (sys.argv[i] == "--map" or sys.argv[i] == "-m") and i != len(sys.argv) - 1:
         mapc = Map(sys.argv[i + 1])
+    elif (sys.argv[i] == "--file" or sys.argv[i] == "-f") and i != len(sys.argv) - 1:
+        fichiero = sys.argv[i + 1]
     else:
         print("Unkown argument", sys.argv[i])
         quit()
--- mainRob3.py
+++ mainRob3.py
@@ -605,7 +605,7 @@ class MyRob(CRobLinkAngs):
             self.driveMotors(power, -power)
 
     def writePath(self, path, order):
-        file = open("pathC3.out", 'w')
+        file = open("path-c3.out", 'w')
 
         for i in range(len(path)):
             if path[i] in self.spots.values():
@@ -654,16 +654,19 @@ rob_name = "pClient1"
 host = "localhost"
 pos = 1
 mapc = None
+ficheiro = ""
 
 for i in range(1, len(sys.argv), 2):
     if (sys.argv[i] == "--host" or sys.argv[i] == "-h") and i != len(sys.argv) - 1:
         host = sys.argv[i + 1]
     elif (sys.argv[i] == "--pos" or sys.argv[i] == "-p") and i != len(sys.argv) - 1:
         pos = int(sys.argv[i + 1])
-    elif (sys.argv[i] == "--robname" or sys.argv[i] == "-p") and i != len(sys.argv) - 1:
+    elif (sys.argv[i] == "--robname" or sys.argv[i] == "-r") and i != len(sys.argv) - 1:
         rob_name = sys.argv[i + 1]
     elif (sys.argv[i] == "--map" or sys.argv[i] == "-m") and i != len(sys.argv) - 1:
         mapc = Map(sys.argv[i + 1])
+    elif (sys.argv[i] == "--file" or sys.argv[i] == "-f") and i != len(sys.argv) - 1:
+        fichiero = sys.argv[i + 1]
     else:
         print("Unkown argument", sys.argv[i])
         quit()
